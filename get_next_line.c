ba3e888dd581d4476208024c2cd43a22cb37cb6b
xadabunu <xadabunu@student.s19.be>
2022-11-22 15:05:57 +0100

    init repo

--- get_next_line.c
+++ get_next_line.c
@@ -6,12 +6,24 @@
 /*   By: xadabunu <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/22 13:25:39 by xadabunu          #+#    #+#             */
-/*   Updated: 2022/11/22 13:28:55 by xadabunu         ###   ########.fr       */
+/*   Updated: 2022/11/22 15:05:36 by xadabunu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <unistd.h>
+#include <stdlib.h>
+
 char	*get_next_line(int fd)
 {
+	char	*buffer;
+	long	ret;
 
+	ret = read(fd, buffer, BUFFER_SIZE);
+	if (ret > 0)
+	{
+		buffer = malloc(sizeof(*buffer) * (ret + 1));
+		assign(res, buffer);
+	}
+	return (res);
 }
 
