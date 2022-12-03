a59bad6ce23aea42a84b442effdadf746093fb51
xadabunu <2907xadabunu@student.s19.be>
2022-12-03 23:48:54 +0100

    update

--- get_next_line.c
+++ get_next_line.c
@@ -10,20 +10,53 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h>
-#include <stdlib.h>
+#include "get_next_line.h"
+
+static int	gnl_len(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] && str[i] != '\n')
+		++i;
+	return (i);
+}
+
+static char	*ft_read_to_n(char *str)
+{
+	int		i;
+	char	*dest;
+	int		len;
+
+	i = 0;
+	len = gnl_len(str)
+	dest = malloc(sizeof(*dest) * (len + 1));
+	if (!dest)
+		return (NULL);
+	while (i < len)
+	{
+		dest[i] = str[i];
+		++i;
+	}
+	dest[i] = str[i];
+	return (dest);
+}
 
 char	*get_next_line(int fd)
 {
-	char	*buffer;
-	long	ret;
+	static char	buffer[BUFFER_SIZE];
+	long		ret;
+	char		*next_line;
 
+	if (fd < 0 || BUFFER_SIZE < 1)
+		return (NULL);
 	ret = read(fd, buffer, BUFFER_SIZE);
-	if (ret > 0)
-	{
-		buffer = malloc(sizeof(*buffer) * (ret + 1));
-		assign(res, buffer);
-	}
-	return (res);
+	// if (ret > 0)
+	// {
+	// }
+	next_line = ft_read_to_n(buffer);
+	if (!next_line)
+		return (NULL);
+	return (next_line);
 }
 
